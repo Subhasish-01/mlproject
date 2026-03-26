3e3c8212d058a8466d8818c6da47f82f92da7cb3
Subhasish-01 <subhasishs111@gmail.com>
2026-03-26 16:02:12 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
       _,_,exe_tb = error_detail.exe_info()
       file_name = exe_tb.tb_frame.f_code.co_file_name
@@ -13,3 +14,4 @@ class CustomException(Exception):
     
       def __str__(self):
             return self.error_message
+
